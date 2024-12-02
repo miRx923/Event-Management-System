0e39c5e07e8a6e843467e6809702b46334370a87
miRx923 <miroslav.kosari@student.tuke.sk>
2024-12-02 16:36:50 +0100

    registracia page hotova

--- EventManagementSystem/EventManagementSystem/Controllers/HomeController.cs
+++ EventManagementSystem/EventManagementSystem/Controllers/HomeController.cs
@@ -59,7 +59,7 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult SignUpPage()
+    public IActionResult Registration()
     {
         return View();
     }
